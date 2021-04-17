b4b2ee63115ce32656ddd74492130583cabb341e
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-04-17 11:51:55 +0200

    18.5

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Simple;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,11 @@ public class TaskController {
         return new ArrayList<>();
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "getSimple")
+    public Simple getSimple(Long simple){
+        return new Simple("Jan", "Kowalski", 24);
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
         return new TaskDto(1L, "test title ", "test_content");
@@ -31,7 +37,7 @@ public class TaskController {
         return new TaskDto(1L, "Edited test title ", "Test content ");
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createTaskg")
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto){
 
     }
---
+++ src/main/java/com/crud/tasks/domain/Simple.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Simple {
+    private String name;
+    private String surnName;
+    private int age;
+}

dd34bf841315dbb4152566ffe38214a05802641f
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-04-20 11:19:15 +0200

    18.5

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,30 +1,30 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.Simple;
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/task")
-public class TaskController {
+public class TaskController  {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-    public List<TaskDto> getTasks(){
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public List<TaskDto> getTask(){
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getSimple")
-    public Simple getSimple(Long simple){
-        return new Simple("Jan", "Kowalski", 24);
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(Long taskId) {
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public TaskDto getTasks(Long taskId) {
         return new TaskDto(1L, "test title ", "test_content");
     }
 
@@ -42,3 +42,4 @@ public class TaskController {
 
     }
 }
+

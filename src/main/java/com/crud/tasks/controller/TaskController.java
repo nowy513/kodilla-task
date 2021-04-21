23989d3ebac02989f506329800952816432e1f2b
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-04-21 12:41:01 +0200

    18.5 - poprawione

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,27 +17,30 @@ import java.util.Optional;
 @RequestMapping("/v1/task")
 public class TaskController  {
 
-    private final DbService service;
-    private final TaskMapper taskMapper;
-
-    @Autowired
-    public TaskController(DbService service, TaskMapper taskMapper) {
-        this.service = service;
-        this.taskMapper = taskMapper;
-    }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-    public List<TaskDto> getTasks() {
-        List<Task> tasks = service.getAllTasks();
-        return taskMapper.mapToTaskDtoList(tasks);
+    public List<TaskDto> getTasks(){
+        return new ArrayList<>();
     }
-
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
-        return taskMapper.mapToTaskDto(
-                service.getTask(taskId).orElseThrow(TaskNotFoundException::new)
-        );
+    public TaskDto getTask(Long taskId) {
+        return new TaskDto(1L, "test title ", "test_content");
     }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
+    public void deleteTask(Long taskedId){
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask(TaskDto taskDto){
+        return new TaskDto(1L, "Edited test title ", "Test content ");
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public void createTask(TaskDto taskDto){
+
+    }
+
 }
 

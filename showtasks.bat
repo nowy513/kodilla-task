d3b4c0d6b01e695afac5cbcc614b86a29a086232
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-05-01 14:20:00 +0200

    21.4

---
+++ showtasks.bat
@@ -0,0 +1,17 @@
+call runcrud
+if "%ERRORLEVEL%" == "0" goto openbrowser
+echo Cannot open runcrud
+echo.
+goto fail
+
+:openbrowser
+start chrome http://localhost:8080/crud/v1/task/getTasks
+goto end
+
+:fail
+echo.
+echo There were errors.
+
+:end
+echo.
+echo Work is finished
